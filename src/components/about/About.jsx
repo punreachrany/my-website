c1bc63dde093ddc8b12d1a9d5db4f1ef0c877632
Punreach Rany <punreach.rany@yahoo.com>
2024-10-10 17:30:39 +0900

    [feat]: About component added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,70 @@
 import React from "react";
 import "./about.css";
+import Image from "../../assets/avatar-2.svg";
+import AboutBox from "./AboutBox";
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className="about container section" id='about'>
+            <h2 className="section__title">About Me</h2>
+            <div className="about__container grid">
+                <img src={Image} alt="" className="about__img" />
+                
+                <div className="about__data grid">
+                    <div className="about__info">
+                        <p className="about__description">
+                        I'm a software developer with 3+ years in mobile, web, and backend development, 
+                        along with hands-on experience in cloud technologies.
+                        </p>
+                        <a href="" className="btn">Download CV</a>
+                    </div>
+
+                    <div className="about__skills grid">
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Development</h3>
+                                <span className="skills__number">90%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage development">
+
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">UI/UX Design</h3>
+                                <span className="skills__number">80%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage ui__design">
+
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Photography</h3>
+                                <span className="skills__number">50%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage photography">
+
+                                </span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <AboutBox />
+        </section>
     )
 }
 
